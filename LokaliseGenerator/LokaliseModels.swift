e90dc48eb83df27c9c1a90c44194c5e63fcf2431
Morgan Collino <mcollino@stashinvest.com>
2020-04-10 15:25:44 -0400

    Models

---
+++ LokaliseGenerator/LokaliseModels.swift
@@ -0,0 +1,21 @@
+//
+//  LokaliseModels.swift
+//  LokaliseGenerator
+//
+//  Created by Morgan Collino on 4/10/20.
+//  Copyright © 2020 Morgan Collino. All rights reserved.
+//
+
+import Foundation
+
+// Lokalise JSON models
+
+struct Singular: Decodable {
+    let notes: String
+    let translation: String
+}
+
+struct Plural: Decodable {
+    let notes: String
+    let translation: [String: String]
+}
